e9676add1def1643afda326ca175c59083a660de
NedraMekni <70708093+NedraMekni@users.noreply.github.com>
2023-05-05 17:47:52 +0200

    refactoring load_graph

--- scripts/train_graph.py
+++ scripts/train_graph.py
@@ -25,6 +25,7 @@ from sklearn.model_selection import KFold
 from functools import reduce
 
 from dotenv import load_dotenv
+import psutil
 load_dotenv()
 ROOT_PATH = os.getenv('ROOT_PATH')
 
@@ -182,19 +183,36 @@ def export_model(model, optimizer, epoch, loss):
             'optimizer_state_dict': optimizer.state_dict(),
             'loss': loss,
             }, fname)
+
+
 def get_dicts():
-    global_G={}
+
     files = os.listdir(ROOT_PATH+'/cached_graph')
     files = [x for x in files if x [-3:]=='.pt']
-
+    print(files)
     files = [ROOT_PATH+'/cached_graph/' + x for x in files] #select all .pt files
 
-    
+    pytg_graph_dict = {}
     for f in files:
-        global_G.update(torch.load(f)) #merge two dicts
-    print(len(global_G.keys()))
-    
-    return global_G
+        local_G= torch.load(f)
+        for k in local_G.keys():
+            outliers = ['1SL3','2I4U','4DJO','4DJR']
+            for k in global_G:
+            if k.split('.')[0] in outliers:
+                print('Identify outlier')
+                continue 
+            feat_k = local_G[k][1] 
+            pytg_graph_dict[k] = local_G[k][0]
+            new_x = [ reduce (lambda x,y:x+y,feat_k[int(el.item())]['attributes']) for el in pytg_graph_dict[k].x]
+
+            pytg_graph_dict[k].x = torch.tensor(new_x, dtype = torch.float)# ,device=device)
+            del feat_k, new_x
+
+        print(f,' loaded')
+        print("RAM used GB: ", psutil.virtual_memory()[3]/1000000000)
+        del local_G
+    print("OK")
+    return pytg_graph_dict
 
 def dissect(uni_graph):
     with open('./prova.txt','w') as f:
@@ -224,26 +242,10 @@ def compress_graph_nodes(graph_list):
 if __name__ == "__main__":
     
     #global_G = torch.load(ROOT_PATH+'/cached_graph/tensorG_0.pt')
-    global_G = get_dicts()
+    pytg_graph_dict = get_dicts()
     print('end loading\n Dataset dimension <nGraphs> = {}'.format(len(global_G.keys())))
     
-    pytg_graph_dict = {}
-    
-    outliers = ['1SL3','2I4U','4DJO','4DJR']
-    for k in global_G:
-        if k.split('.')[0] in outliers:
-            print('Identify outlier')
-            continue 
 
-        feat_k = global_G[k][1] 
-        pytg_graph_dict[k] = global_G[k][0]
-        print(k)
-        new_x = [ reduce (lambda x,y:x+y,feat_k[int(el.item())]['attributes']) for el in pytg_graph_dict[k].x]
-
-        pytg_graph_dict[k].x = torch.tensor(new_x, dtype = torch.float)# ,device=device)
-
-    del global_G
-    print('end loading pytg_graph_dict')
     """
     Function for training GCN model
     """
@@ -263,6 +265,8 @@ if __name__ == "__main__":
         if new_g.edge_index.size(dim=1)<= 4000000:
             graph_list.append(new_g) 
             graph_y.append(pytg_graph_dict[k].y)
+    model_num_feature = pytg_graph_dict[k0].num_node_features
+    del pytg_graph_dict
     
     batch_size = 6
     print(len(graph_list))
@@ -271,9 +275,7 @@ if __name__ == "__main__":
     graph_y = graph_y[:limit_g]
     print(limit_g)
 
-    model_num_feature = pytg_graph_dict[k0].num_node_features
-    del pytg_graph_dict
-    del global_G
+
 
 
     #seed = 9
